61787b5790f36e6628df41a954db223ee2795251
FarhanFardid <farhanfardid009@gmail.com>
2023-04-01 11:11:24 +0600

    data updated

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Blog.css';
 
-const Blog = () => {
+const Blog = (props) => {
+    const {id,Author_img,Author_name,Blog_title,Blog_cover,Read_time,Publish} = props.blog;
     return (
-        <div>
-            
+        <div className='blog'>
+            <img className='blog-img' src={Blog_cover} alt="blog cover image" />
+            <div className='blog-info'>
+                <div className='author-img'>
+                    <img src={Author_img} alt="author image"  />
+                </div>
+            </div>
         </div>
     );
 };
